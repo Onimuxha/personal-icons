9b5bc9c790cd9b50f7e44272e4300b303de00652
Socheath <freshlume@gmail.com>
2025-09-06 16:51:54 +0700

    aldsj

---
+++ tsub.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/lib/index.ts'],
+  format: ['cjs', 'esm'],
+  dts: true,
+  clean: true,
+  sourcemap: true,
+});
